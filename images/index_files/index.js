da65396d4d120329f02b2deae0cf934017a33f69
Your NameAlexander <alexgue13@gmail.com>
2018-02-20 15:18:00 -0500

    Added nav bar and redesigned capabilites and skills row

---
+++ images/index_files/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+		$("#barWrapper").animate({'margin-left': '+=125%'}, 3000);
+		$("#downWrapper").animate({'margin-top': '+=100%'}, 3000);
+
+});
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-		$("#barWrapper").animate({'margin-left': '+=125%'}, 3000);
-		$("#downWrapper").animate({'margin-top': '+=100%'}, 3000);
+		$("#barWrapper").animate({'margin-left': '+=125%'}, 3500);
+		$("#downWrapper").animate({'margin-top': '+=108vh'}, 3500);
 
 });
