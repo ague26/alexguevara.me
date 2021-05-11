b30fd006047b95d5ffb17d856fc130c58744834d
ague26 <alexgue26@hotmail.com>
2021-05-11 16:29:03 -0400

    Delete alexguevaraV1 directory
    Takes up loading time

--- alexguevaraV1/images/index_files/index.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-		$("#barWrapper").animate({'margin-left': '+=125%'}, 3000);
-		$("#downWrapper").animate({'margin-top': '+=100%'}, 3000);
-
-});
--- alexguevaraV1/index.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-		$("#barWrapper").animate({'margin-left': '+=125%'}, 3500);
-		$("#downWrapper").animate({'margin-top': '+=108vh'}, 3500);
-
-});
